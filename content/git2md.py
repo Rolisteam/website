bc19e6ef64104e912f777f8bc23670173233e2fb
Renaud G <renaud@rolisteam.org>
2020-03-10 01:03:53 +0100

    improve git2md.py

--- content/git2md.py
+++ content/git2md.py
@@ -6,10 +6,12 @@ import tempfile
 from git import Repo
 from datetime import datetime
 
+def cleanUp(homedir):
+    shutil.rmtree(homedir, ignore_errors=True)
 
-def cleanUp(homedir,langs):
-    for lang in langs:
-        shutil.rmtree("{}{}".format(homedir,lang), ignore_errors=True)
+#def cleanUp(homedir,langs):
+#    for lang in langs:
+ #        shutil.rmtree("{}{}".format(homedir,lang), ignore_errors=True)
 
 def clone(url, dirroot):
     return Repo.clone_from(url, dirroot)
@@ -65,21 +67,28 @@ def buildPage(paths, lang, tag, homedir, workdir):
     sheets=paths[0]
     themes=paths[1]
 
-    f = open("community/community_{}_{}.md".format(lang,tag),"a")
-    f.write("Title: Content\n        Date: {}\n        slug: community02\n        status: hidden\n        lang: {}".format(datetime.now().isoformat(' ',timespec='minutes'),lang))
+    f = open("pages/community/community_{}_{}.md".format(lang,tag),"a")
+    f.write("title: Content\ndate: {}\nslug: community{}\nstatus: hidden\nlang: {}\n\n".format(datetime.now().isoformat(' ',timespec='minutes'),tag,lang))
 
 
-    f.write("\n# {}".format(trad["sheettitle"]))
+    f.write("\n# {}\n".format(trad["sheettitle"]))
 
     for sheet in sheets:
-        f.write("\n* [{}]({})".format(os.path.basename(os.path.normpath(sheet)),sheet))
+        f.write("\n* [{}]({{static}}{})".format(os.path.basename(os.path.normpath(sheet)),sheet.replace(homedir+"/","")))
 
-    f.write("\n# {}".format(trad["themetitle"]))
+    f.write("\n\n# {}\n".format(trad["themetitle"]))
     
     for theme in themes:
-        f.write("\n* [{}]({})".format(os.path.basename(os.path.normpath(theme)),theme))
+        f.write("\n* [{}]({{static}}{})".format(os.path.basename(os.path.normpath(theme)),theme.replace(homedir+"/","")))
         
+def buildIndexPage(langs, tags, homedir):
 
+    for lang in langs:
+        f = open("pages/community/index_{}.md".format(lang),"a")
+        f.write("Title: Community Content\nDate: {}\nslug: contentindex\nlang: {}\n\n".format(datetime.now().isoformat(' ',timespec='minutes'),lang))
+        f.write("## Content\n")
+        for tag in tags:
+            f.write("[Community data for {}]({{filename}}community_{}_{}.md)  \n".format(tag,lang,tag))
 
 
 
@@ -92,7 +101,7 @@ def processConfig(lang, tag, homedir, workdir, repo):
 
 def main():
     workdir=tempfile.mkdtemp()
-    homedir="/home/renaud/www/rolisteam/content/community"
+    homedir="/home/renaud/www/rolisteam/content/pages/community"
     gitrepo="https://invent.kde.org/kde/rolisteam-community-data.git"
     langs=['fr','en']
     tags=['v1.8','v1.9']
@@ -102,7 +111,7 @@ def main():
     print("Workdir {}".format(workdir))
 
     print("clean up")
-    cleanUp(homedir, langs)
+    cleanUp(homedir)
 
     print("clone repository")
     repo=clone(gitrepo, workdir)
@@ -114,7 +123,8 @@ def main():
             print("Processing lang:{} - tag:{}".format(lang,tag))
             processConfig(lang,tag, homedir, workdir, repo)
 
-
+    print("build index page")
+    buildIndexPage(langs,tags,homedir)
     shutil.rmtree(workdir)
 
 
